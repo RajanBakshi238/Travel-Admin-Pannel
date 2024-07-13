49498a9eae9d2c93ed93f3df1ce6954c1cd3148b
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-07-14 01:30:01 +0530

    wip:create-trip-formik-version

--- src/contracts/ICreateTripRequest.ts
+++ src/contracts/ICreateTripRequest.ts
@@ -1,9 +1,17 @@
 export interface ICreateTripRequest {
-    place:  string,
-    startDate: string,
-    endDate: string,
-    pickup: string,
-    termsAndConditions: string,
-    enquiryNumber: string,
-    price: number
+  place: string;
+  startDate: string;
+  endDate: string;
+  pickup: string;
+  termsAndConditions: [string];
+  enquiryNumber: string;
+  price: number;
+  inclusions: [string];
+  exclusions: [string];
+  itinerary: Iitinerary;
+}
+
+interface Iitinerary {
+  day: number;
+  description: [string];
 }
--- src/pages/Dashboard/CreateTrip/index.tsx
+++ src/pages/Dashboard/CreateTrip/index.tsx
@@ -1,80 +1,259 @@
 import { Form, InputGroup } from "react-bootstrap";
+import { useFormik, FormikProvider, Form as FormikForm, FieldArray } from 'formik'
 import "./style.scss"
 
 const CreateTrip = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            place: "",
+            startDate: "",
+            endDate: "",
+            pickup: "",
+            termsAndConditions: [''],
+            price: null,
+            inclusions: [''],
+            exclusions: [''],
+            itinerary: [{ day: 1, description: [] }]
+        },
+        onSubmit: (values) => {
+            console.log(values, "...... valuess")
+        }
+    })
+
+
+    const { handleChange, handleBlur, values } = formik
+    console.log(values, "........")
     return (
         <div className="register-as-page">
-            <div className="form-box">
-                <div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-map-marker-alt"></i></InputGroup.Text>
-                        <Form.Control
-                            placeholder="Place"
-                            aria-label="Place"
-                            aria-describedby="basic-addon1"
-                        />
-                    </InputGroup>
-                </div>
-                <div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-truck-pickup"></i></InputGroup.Text>
-                        <Form.Control
-                            placeholder="Pick up"
-                            aria-label="pick up"
-                            aria-describedby="basic-addon1"
-                        />
-                    </InputGroup>
-                </div>
+            <FormikProvider value={formik} >
+                <FormikForm onSubmit={formik.handleSubmit}>
+                    <div className="form-box">
+                        <div>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><i className="fas fa-map-marker-alt"></i></InputGroup.Text>
+                                <Form.Control
+                                    name="place"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Place"
+                                    aria-label="Place"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </div>
+                        <div>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><i className="fas fa-truck-pickup"></i></InputGroup.Text>
+                                <Form.Control
+                                    name="pickup"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Pick up"
+                                    aria-label="pick up"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </div>
 
-                <div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-calendar-week"></i></InputGroup.Text>
-                        <Form.Control
-                            type="date"
-                            placeholder="Start Date"
-                            aria-label="start date"
-                            aria-describedby="basic-addon1"
-                        />
-                    </InputGroup>
-                </div>
-                <div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-calendar-week"></i></InputGroup.Text>
-                        <Form.Control
-                            type="date"
-                            placeholder="End Date"
-                            aria-label="End date"
-                            aria-describedby="basic-addon1"
-                        />
-                    </InputGroup>
-                </div>
-                <div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-phone-volume"></i></InputGroup.Text>
-                        <Form.Control
-                            placeholder="Enquiry Number"
-                            aria-label="pick up"
-                            aria-describedby="basic-addon1"
-                        />
-                    </InputGroup>
-                </div>
-                <div>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1"><i className="fas fa-dollar-sign"></i></InputGroup.Text>
-                        <Form.Control
-                            placeholder="Price"
-                            aria-label="pick up"
-                            aria-describedby="basic-addon1"
-                        />
-                    </InputGroup>
-                </div>
+                        <div>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><i className="fas fa-calendar-week"></i></InputGroup.Text>
+                                <Form.Control
+                                    name="startDate"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    type="date"
+                                    placeholder="Start Date"
+                                    aria-label="start date"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </div>
+                        <div>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><i className="fas fa-calendar-week"></i></InputGroup.Text>
+                                <Form.Control
+                                    name="endDate"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    type="date"
+                                    placeholder="End Date"
+                                    aria-label="End date"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </div>
+                        <div>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><i className="fas fa-phone-volume"></i></InputGroup.Text>
+                                <Form.Control
+                                    name="enquiryNumber"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Enquiry Number"
+                                    aria-label="pick up"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </div>
+                        <div>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1"><i className="fas fa-dollar-sign"></i></InputGroup.Text>
+                                <Form.Control
+                                    name="price"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Price"
+                                    aria-label="pick up"
+                                    aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                        </div>
+                        <div>
+                            <FieldArray
+                                name="inclusions"
+                                render={arrayHelpers => (
+                                    <div>
+                                        {values.inclusions && values.inclusions.length > 0 ? <>
+                                            {values.inclusions.map((inclusion, index) => {
+                                                return <InputGroup key={index} className="mb-3 dynamic-no-input">
+                                                    {/* <InputGroup.Text id="basic-addon1"><i className="fas fa-dollar-sign"></i></InputGroup.Text> */}
+                                                    <Form.Control
+                                                        name={`inclusions.${index}`}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        placeholder="Inclusion"
+                                                        aria-label="pick up"
+                                                        aria-describedby="basic-addon1"
+                                                    />
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-outline-primary add-btn"
+                                                        onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                                                    >
+                                                        -
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-outline-primary remove-btn"
+                                                        type="button"
+                                                        onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                                                    >
+                                                        +
+                                                    </button>
+                                                </InputGroup>
 
-            </div>
-            <div className="submit-box">
-                <button className="btn btn-primary">
-                    Create trip
-                </button>
-            </div>
+                                            })}
+
+                                        </> : <>
+                                            <button className="btn btn-outline-primary" type="button" onClick={() => arrayHelpers.push('')}>
+                                                Add a Inclusion
+                                            </button>
+                                        </>}
+
+                                    </div>
+                                )}
+                            />
+                        </div>
+                        <div>
+                            <FieldArray
+                                name="exclusions"
+                                render={arrayHelpers => (
+                                    <div>
+                                        {values.exclusions && values.exclusions.length > 0 ? <>
+                                            {values.exclusions.map((exclusion, index) => {
+                                                return <InputGroup key={index} className="mb-3 dynamic-no-input">
+                                                    {/* <InputGroup.Text id="basic-addon1"><i className="fas fa-dollar-sign"></i></InputGroup.Text> */}
+                                                    <Form.Control
+                                                        name={`exclusions.${index}`}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        placeholder="Exclusion"
+                                                        aria-label="pick up"
+                                                        aria-describedby="basic-addon1"
+                                                    />
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-outline-primary add-btn"
+                                                        onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                                                    >
+                                                        -
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-outline-primary remove-btn"
+                                                        type="button"
+                                                        onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                                                    >
+                                                        +
+                                                    </button>
+                                                </InputGroup>
+
+                                            })}
+
+                                        </> : <>
+                                            <button className="btn btn-outline-primary" type="button" onClick={() => arrayHelpers.push('')}>
+                                                Add a exclusions
+                                            </button>
+                                        </>}
+
+                                    </div>
+                                )}
+                            />
+                        </div>
+                        <div>
+                            <FieldArray
+                                name="termsAndConditions"
+                                render={arrayHelpers => (
+                                    <div>
+                                        {values.termsAndConditions && values.termsAndConditions.length > 0 ? <>
+                                            {values.termsAndConditions.map((terms, index) => {
+                                                return <InputGroup key={index} className="mb-3 dynamic-no-input">
+                                                    {/* <InputGroup.Text id="basic-addon1"><i className="fas fa-dollar-sign"></i></InputGroup.Text> */}
+                                                    <Form.Control
+                                                        name={`termsAndConditions.${index}`}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        placeholder="Terms and conditions"
+                                                        aria-label="pick up"
+                                                        aria-describedby="basic-addon1"
+                                                    />
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-outline-primary add-btn"
+                                                        onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
+                                                    >
+                                                        -
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-outline-primary remove-btn"
+                                                        type="button"
+                                                        onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
+                                                    >
+                                                        +
+                                                    </button>
+                                                </InputGroup>
+
+                                            })}
+
+                                        </> : <>
+                                            <button className="btn btn-outline-primary" type="button" onClick={() => arrayHelpers.push('')}>
+                                                Add a termsAndConditions
+                                            </button>
+                                        </>}
+
+                                    </div>
+                                )}
+                            />
+                        </div>
+
+                    </div>
+                    <div className="submit-box">
+                        <button className="btn btn-primary">
+                            Create trip
+                        </button>
+                    </div>
+                </FormikForm>
+            </FormikProvider>
         </div>
     );
 }
