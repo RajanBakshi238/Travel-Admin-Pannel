5f9f6cb10c276d90360462a2c24ae2b27e9ba78a
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-07-30 02:43:49 +0530

    image upload done

--- src/components/common/FormElements/ImageInput/index.tsx
+++ src/components/common/FormElements/ImageInput/index.tsx
@@ -25,18 +25,39 @@ const ImageInput: React.FC<IImageInput> = ({ name, label, className, value, inpu
     console.log(values, ">>>valies")
     const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files[0]) {
-            uploadFile({ file: e.target.files[0] }).unwrap().then((res) => {
-                console.log(res, "?>>>>>>>>>> file uplokad response")
+            console.log(e.target.files, ">>>>>> rachna")
+            // @TODO check here that all files shoul be image
+
+            const imagePromise = Array.from(e.target.files)?.map((file) => {
+                return uploadFile({ file: file }).unwrap()
+            })
+
+
+            Promise.all(imagePromise).then((res) => {
+                console.log(res, ">>>>> Image promise response")
                 if (multiple) {
-                    setFieldValue(name, [...((values as { [key: string]: string })?.[name]), res])
+                    setFieldValue(name, [...((values as { [key: string]: string })?.[name]), ...res])
                 } else {
-                    setFieldValue(name, res)
+                    setFieldValue(name, res[0])
 
                 }
 
-            }).catch((err) => {
-                console.log(err, ">>>>> err of file upload ")
+
+            }).catch((error) => {
+                console.log(error, "Image promise error.")
             })
+            // uploadFile({ file: e.target.files[0] }).unwrap().then((res) => {
+            //     console.log(res, "?>>>>>>>>>> file uplokad response")
+            //     if (multiple) {
+            //         setFieldValue(name, [...((values as { [key: string]: string })?.[name]), res])
+            //     } else {
+            //         setFieldValue(name, res)
+
+            //     }
+
+            // }).catch((err) => {
+            //     console.log(err, ">>>>> err of file upload ")
+            // })
         }
     }
 
@@ -51,7 +72,7 @@ const ImageInput: React.FC<IImageInput> = ({ name, label, className, value, inpu
             <div className='custom-image'>
                 <label htmlFor={name} className='btn btn-primary'>{inputLabel}</label>
             </div>
-            <input id={name} hidden type="file" onChange={handleUpload} onBlur={handleBlur} name={name} />
+            <input id={name} multiple hidden type="file" onChange={handleUpload} onBlur={handleBlur} name={name} />
             <div className='image-list'>
                 {multiple ?
                     <>{((values as { [key: string]: string | [] })?.[name] as [])?.map((item: IFileResponse, index: number) => {
@@ -59,14 +80,14 @@ const ImageInput: React.FC<IImageInput> = ({ name, label, className, value, inpu
 
                             <div key={index}>
                                 <div className='cut' onClick={() => handleRemove(item.id)}>X</div>
-                                <img className='uploaded-img' src={`http://localhost:3005/${item.path}`} />
+                                <img className='uploaded-img' src={`http://localhost:3001/${item.path}`} />
                             </div>
                         </>
                     })}</>
                     : <>
                         {!!(values as any)?.[name]?.id && <div>
                             <div className='cut' onClick={() => setFieldValue(name, null)}>X</div>
-                            <img className='uploaded-img' src={`http://localhost:3005/${(values as any)?.[name]?.path}`} />
+                            <img className='uploaded-img' src={`http://localhost:3001/${(values as any)?.[name]?.path}`} />
                         </div>}
                     </>
                 }
