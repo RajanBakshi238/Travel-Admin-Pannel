e205cec522bd9d93d5425e17642b8a463ad109d5
RajanBakshi238 <rajan.bakshi238@gmail.com>
2024-08-15 19:22:10 +0530

    travellers section addes

--- src/components/landingPage/AboutUs.tsx
+++ src/components/landingPage/AboutUs.tsx
@@ -1,3 +1,4 @@
+import "./styles.scss"
 
 const AboutUs = () => {
     return (
@@ -5,10 +6,13 @@ const AboutUs = () => {
             <div className="container-xxl py-5" id="yatri-about">
                 <div className="container">
                     <div className="row g-5">
-                        <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style={{ minHeight: "400px" }}>
-                            <div className="position-relative h-100">
-                                <img className="img-fluid position-absolute w-100 h-100" src="img/about.jpg" alt="" style={{ objectFit: "cover" }} />
-                            </div>
+                        <div className="col-lg-6 wow fadeInUp d-flex align-items-center justify-content-center" data-wow-delay="0.1s">
+                            {/* <div className="position-relative h-100 "> */}
+                                <div className="ty-ring">
+
+                                    <img className="traveller-img  img-fluid" src="img/about.jpg" alt="" style={{ objectFit: "cover" }} />
+                                </div>
+                            {/* </div> */}
                         </div>
                         <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                             <h6 className="section-title bg-white text-start text-primary pe-3">About Us</h6>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import AboutUs from '../components/landingPage/AboutUs'
 import Destination from '../components/landingPage/Destination'
 import ContactUs from '../components/landingPage/ContactUs'
 import Footer from '../components/landingPage/Footer'
+import Travellers from '../components/landingPage/travellers'
 
 const Home = () => {
     return (
         <>
             <Header />
             <AboutUs />
+            <Travellers />
             <Destination />
             <ContactUs />
             <Footer />
